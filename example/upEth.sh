9676772581bcc47de05c732e0a4452cacf1b671a
Kluchnik <44801636+kluchnik@users.noreply.github.com>
2022-10-10 17:17:00 +0300

    Delete /example/upEth.sh

--- example/upEth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Активация всех сетевых интерфейсов
-
-eth_list=$(ip a | awk -F': ' '{print $2}' | tr -s '\n')
-
-for eth in ${eth_list}; do
-  ip link set dev ${eth} up;
-  ip link show dev ${eth} | grep -E '^[0-9]*:';
-done
